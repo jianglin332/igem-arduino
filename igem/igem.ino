a89e27dee3d28dac504a5b1eb5e0d5e23d4862a2
David Jiang <jianglin332@outlook.com>
2017-08-16 17:47:54 +0800

    modified:   igem.ino 	deleted:    light/IndicatorLED.h

--- igem/igem.ino
+++ igem/igem.ino
@@ -1,21 +1,35 @@
 #define DEBUG
-const int IN1 = 3;
-const int IN2 = 4;
-const int EN = 5;
-const int pwm = 6;
 
-const int light = 7;
+#include "IndicatorLED.h"
+
+const int IN1[2] = {3, 4};
+const int IN2[2] = {7, 8};
+const int EN[2] = {5, 6};
+
+const int light = 12;
+
+
+const int ON = 1;
+const int OFF = 0;
+
+IndicatorLED *ind;
 
 const int UP = 1;
 const int DOWN = 2;
-// UP & DOWN: #[1/2]$SPEED%
+// UP & DOWN: #[1/2]$NO%SPEED&
 const int STOP = 3;
 //STOP: #3$%
 const int LIGHT = 4;
 //LIGHT: #4$STATE%
 
 //ascii !"#$%&'()*+,-./
-const int dataSize = 2;
+const int dataSize = 3;
+char Read()
+{
+  char t = Serial.read();
+  delay(3);
+  return t;
+}
 struct data
 {
   int dat[dataSize];
@@ -31,10 +45,15 @@ struct data
       char nowCh = '#' + i;
       int num = 0;
       while (Serial.available() > 0 && nowByte != nowCh)
-        nowByte = Serial.read();
-      while (Serial.available() > 0 && (nowByte = Serial.read()) != nowCh + 1)
+        nowByte = Read();
+      Serial.println(nowByte);
+      delay(1);
+      while (Serial.available() > 0 && (nowByte = Read()) != nowCh + 1)
         num = num * 10 + nowByte - '0';
       dat[i] = num;
+      Serial.print(i);
+      Serial.print(" ");
+      Serial.println(num);
     }
   }
   int operator[](int p)
@@ -46,25 +65,30 @@ struct data
 unsigned int spd = 0; //Speed
 void setup() {
   // put your setup code here, to run once:
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(pwm, OUTPUT);
-  pinMode(EN, OUTPUT);
+  for (int i = 0; i < 2; i++)
+  {
+    pinMode(IN1[i], OUTPUT);
+    pinMode(IN2[i], OUTPUT);
+    pinMode(EN[i], OUTPUT);
+  }
+  ind = new IndicatorLED;
+  ind->turn(ON);
+  ind->setColor(yellow);
   pinMode(light, OUTPUT);
   Serial.begin(9600);
 }
 
-void setSpd(int a) //Set Speed
+void setSpd(int a, int no) //Set Speed
 {
   spd = a;
-  analogWrite(pwm, spd);
+  analogWrite(EN[no], spd);
 #ifdef DEBUG
   Serial.print("Speed set to ");
   Serial.println(spd);
 #endif
 }
 
-void setMotor(int state, int sp)
+void setMotor(int state, int no, int sp)
 {
   /* * * * * * * *
    *     IN1 IN2 *
@@ -72,19 +96,17 @@ void setMotor(int state, int sp)
    * DOWN 0   1  *
    * STOP 0   0  * 
    * * * * * * * */
-  digitalWrite(IN1, state == UP ? HIGH : LOW);
-  digitalWrite(IN2, state == DOWN ? HIGH : LOW);
-  setSpd(sp);
+  digitalWrite(IN1[no], state == UP ? HIGH : LOW);
+  digitalWrite(IN2[no], state == DOWN ? HIGH : LOW);
+  setSpd(sp, no);
 }
 
 int lightState = 0;
 
-const int ON = 1;
-const int OFF = 0;
-
 void setLight(int state)
 {
   digitalWrite(light, state == ON ? HIGH : LOW);
+  ind->setColor(state == ON ? red : green);
 #ifdef DEBUG
   Serial.print("Light state changed to: ");
   Serial.println(state == ON ? "on" : "off");
@@ -107,7 +129,7 @@ void loop() {
     data now;
     now.read();
     if (isMotor(now[0]))
-      setMotor(now[0], now[1]);
+      setMotor(now[0], now[1], now[2]);
     else if (isLight(now[0]))
       setLight(now[1]);
   }
